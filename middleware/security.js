f495caf9db1064dd822ab9020e9c3b8a3fd03bb5
Mariana Pantelic <mari27line@yahoo.com>
2021-06-29 11:51:45 +0200

    updated cors

--- middleware/security.js
+++ middleware/security.js
@@ -1,5 +1,5 @@
 exports.setCors = (req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.header("Access-Control-Allow-Origin", "*");
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, x-Requested-With, Content-Type, Accept, x-auth"
